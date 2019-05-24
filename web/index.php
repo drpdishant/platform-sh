0af9061b98d5cf6bb5b227f7a7125cf56db0ea59
drpdishant <drpdishant@gmail.com>
2019-05-24 19:20:11 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
-echo "This is a test Page for Plaform.sh\n";
-echo "This is made on github and integrated to platform.sh\n\n";
+echo "This is a test Page for Plaform.sh";
+echo "<br>";
+echo "This is made on github and integrated to platform.sh";
+echo "<br>";
 echo "Hello Planet Earth from Planet Mars!"
 
 ?>
